909e522f151f9088d8b3475916621a697de5a11b
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-11-18 10:37:39 -0500

    small note

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
-
+// api routes
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
